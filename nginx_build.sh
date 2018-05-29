a6530be48eddd4a208c20c3b7110388e9338ec55
karpenkoKhAI <31684017+karpenkoKhAI@users.noreply.github.com>
2018-05-30 00:58:22 +0300

    Update nginx_build.sh

--- nginx_build.sh
+++ nginx_build.sh
@@ -1,154 +1,55 @@
 #!/bin/bash
-#Install 
-apt-get install -y wget unzip make libssl-dev libpcre3-dev gcc make zlib1g-dev >> /dev/null
-#Download source code function
-create_nginx_user() {  
-  if [ ! -z $IS_ALPINE ]; then 
-    getent group nginx || addgroup nginx
-    getent passwd nginx || adduser -G nginx -S -H -s /sbin/nologin nginx 
-  else
-    getent group nginx || groupadd nginx
-    getent user nginx || useradd -g nginx --system --no-create-home nginx 
-  fi
-}
 
-remove_alpine_packages() {
-  apk del /tmp/.build-deps
-}
+wget http://nginx.org/download/nginx-1.9.12.tar.gz
 
-download_extract()
-{
-    URL=$1
-    FILENAME=$2
-    FOLDER=$3
-    EXTRACT=$4
+tar xvf nginx-1.9.12.tar.gz
 
-    if [ ! -d $FOLDER ]; then
-        mkdir -p $FOLDER
-    fi
+cd nginx-1.9.12/
+sudo apt-get install -y curl build-essential make gcc libpcre3 libpcre3-dev libpcre++-dev zlib1g-dev libbz2-dev libxslt1-dev libxml2-dev libgd2-xpm-dev libgeoip-dev libgoogle-perftools-dev libperl-dev libssl-dev libcurl4-openssl-dev wget unzip make libssl-dev libpcre3-dev gcc make zlib1g-dev
 
-    if [ ! -d $EXTRACT ]; then
-        mkdir -p $EXTRACT
-    fi
-
-    if [ ! -f "$FOLDER/$FILENAME" ]; then
-        echo "Downloading $URL to $FOLDER/$FILENAME"
-        wget --quiet  --force-directories --output-document="$FOLDER/$FILENAME" $URL
-    fi
-
-    if [ ! -z "$EXTRACT" ]; then
-        echo "Extracting $FOLDER/$FILENAME to $EXTRACT"
-        case $FILENAME in
-          *.zip) unzip -q -o -d "$EXTRACT" "$FOLDER/$FILENAME";;
-          *.tar.gz) tar xf "$FOLDER/$FILENAME" -C "$EXTRACT";;
-          *) echo "Unknown file format for $FILENAME";;
-        esac
-
-        FILECOUNT=$(ls -al $EXTRACT | wc -l | tr " " "\0")
-        if [ "$FILECOUNT" = "4" ]; then
-           # We have a single folder and need to move content to upper folder
-           FOLDER_NAME=$(ls $EXTRACT | tr " " "\0")
-           mv $EXTRACT/$FOLDER_NAME/* $EXTRACT/
-           rm -rf $EXTRACT/$FOLDER_NAME
-        fi
-    fi
-}
-
-LUA_JIT_VERSION=${LUA_JIT_VERSION:-2.0.4}
-NGINX_VERSION=${NGINX_VERSION:-1.11.3}
-NGINX_DEVEL_VERSION=${NGINX_DEVEL_VERSION:-v0.3.0}
-
-# use blank value for NGINX_MODULE_TYPE if you want modules to be statically compiled
-NGINX_MODULE_TYPE=${NGINX_MODULE_TYPE:-}
-
-LUA_JIT_FILE=LuaJIT-${LUA_JIT_VERSION}.tar.gz
-NGINX_FILE=nginx-$NGINX_VERSION.tar.gz
-NGINX_DEVEL_FILE=nginx_devel_kit_${NGINX_DEVEL_VERSION}.tar.gz
-
-LUA_JIT_URL=http://luajit.org/download/LuaJIT-${LUA_JIT_VERSION}.tar.gz
-NGINX_URL=http://nginx.org/download/$NGINX_FILE
-NGINX_DEVEL_URL=https://github.com/simpl/ngx_devel_kit/archive/${NGINX_DEVEL_VERSION}.tar.gz
-
-
-LUA_CJSON_FILE=lua_cjson.zip
-LUA_CJSON_URL=https://github.com/efelix/lua-cjson/archive/master.zip
-
-LUA_MAIN_URL=https://github.com/openresty/lua-nginx-module/archive/master.zip
-LUA_MAIN_FILE=lua-nginx-module.zip
-
-LUA_KAFKA_URL=https://github.com/doujiang24/lua-resty-kafka/archive/master.zip
-LUA_KAFKA_FILE=lua-resty-kafka.zip
-
-LUA_SYSLOG_URL=https://gitlab.com/lsyslog/lsyslog/repository/archive.zip?ref=master
-LUA_SYSLOG_FILE=lua-syslog.zip
-
-LUA_ECHO_URL=https://github.com/openresty/echo-nginx-module/archive/master.zip
-LUA_ECHO_FILE=lua-echo.zip
-
-
-download_extract $NGINX_URL $NGINX_FILE downloads extracts/nginx-${NGINX_VERSION}
-download_extract $NGINX_DEVEL_URL $NGINX_DEVEL_FILE downloads extracts/modules/ngx_devel_kit
-
-download_extract $LUA_CJSON_URL $LUA_CJSON_FILE downloads extracts/deps/lua_cjson
-download_extract $LUA_SYSLOG_URL $LUA_SYSLOG_FILE downloads extracts/deps/lua-syslog
-
-download_extract $LUA_MAIN_URL $LUA_MAIN_FILE downloads extracts/modules/lua-nginx-module
-download_extract $LUA_ECHO_URL $LUA_ECHO_FILE downloads extracts/modules/lua-echo-module
-
-#if [ -z $IS_ALPINE ]; then
-   download_extract $LUA_JIT_URL $LUA_JIT_FILE downloads extracts/deps/luajit
-   
-   make -C extracts/deps/luajit install
-#fi
-
-export LUAJIT_LIB=/usr/local/lib 
-export LUAJIT_INC=/usr/local/include/luajit-2.0
-export LUA_INCLUDE_DIR=$LUAJIT_INC
-export LUA_LIB_DIR=$LUAJIT_LIB
-
-#make -C extracts/deps/lua-syslog 
-make -C extracts/deps/lua_cjson
-
-
-cd extracts/nginx-${NGINX_VERSION}
-LUAJIT_LIB=/usr/local/lib LUAJIT_INC=/usr/local/include/luajit-2.0 \
 ./configure \
---user=nginx                          \
---group=nginx                         \
---prefix=/etc/nginx                   \
---sbin-path=/usr/sbin/nginx           \
---conf-path=/etc/nginx/nginx.conf     \
---pid-path=/var/run/nginx.pid         \
---lock-path=/var/run/nginx.lock       \
---error-log-path=/var/log/nginx/error.log \
---http-log-path=/var/log/nginx/access.log \
---with-http_gzip_static_module        \
---with-http_stub_status_module        \
---with-http_ssl_module                \
---with-pcre                           \
---with-file-aio                       \
---with-http_realip_module             \
---without-http_scgi_module            \
---without-http_uwsgi_module           \
---without-http_fastcgi_module ${NGINX_DEBUG:+--debug} \
---with-cc-opt=-O2 --with-ld-opt='-Wl,-rpath,/usr/local/lib' \
---add${NGINX_MODULE_TYPE}-module=$PWD/../modules/ngx_devel_kit \
---add${NGINX_MODULE_TYPE}-module=$PWD/../modules/lua-nginx-module \
---add${NGINX_MODULE_TYPE}-module=$PWD/../modules/lua-echo-module
+--prefix=/etc/nginx                                                \
+--sbin-path=/usr/sbin/nginx                                        \
+--conf-path=/etc/nginx/nginx.conf                                  \
+--error-log-path=/var/log/nginx/error.log                          \
+--http-log-path=/var/log/nginx/access.log                          \
+--pid-path=/var/run/nginx.pid                                      \
+--lock-path=/var/run/nginx.lock                                    \
+--http-client-body-temp-path=/var/cache/nginx/client_temp          \
+--http-proxy-temp-path=/var/cache/nginx/proxy_temp                 \
+--http-fastcgi-temp-path=/var/cache/nginx/fastcgi_temp             \
+--http-uwsgi-temp-path=/var/cache/nginx/uwsgi_temp                 \
+--http-scgi-temp-path=/var/cache/nginx/scgi_temp                   \
+--user=nginx                                                       \
+--group=nginx                                                      \
+--with-http_ssl_module                                             \
+--with-http_realip_module                                          \
+--with-http_addition_module                                        \
+--with-http_sub_module                                             \
+--with-http_dav_module                                             \
+--with-http_flv_module                                             \
+--with-http_mp4_module                                             \
+--with-http_gunzip_module                                          \
+--with-http_gzip_static_module                                     \
+--with-http_random_index_module                                    \
+--with-http_secure_link_module                                     \
+--with-http_stub_status_module                                     \
+--with-http_auth_request_module                                    \
+--with-mail                                                        \
+--with-mail_ssl_module                                             \
+--with-file-aio                                                    \
+--with-http_v2_module                                              \
+--with-ipv6                                                        \
+--with-threads                                                     \
+--with-stream                                                      \
+--with-stream_ssl_module                                           \
+--with-http_slice_module
 
-make -j 4
-make install
+make
+sudo make install
 
-create_nginx_user
+sudo useradd -r nginx
+sudo mkdir /var/cache/nginx
+sudo touch /var/cache/nginx/client_temp
 
-#nginx -t
-
-#if [ ! -z $IS_ALPINE ]; then 
-#  remove_alpine_packages
-#fi 
-
-
-
-
-
-
+sudo nginx
