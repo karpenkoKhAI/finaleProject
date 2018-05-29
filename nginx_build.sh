3834ef07c7be69722e1d2d1b25304560eefe0b37
karpenkoKhAI <31684017+karpenkoKhAI@users.noreply.github.com>
2018-05-30 00:58:58 +0300

    Update nginx_build.sh

--- nginx_build.sh
+++ nginx_build.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
+sudo apt-get install -y curl build-essential make gcc libpcre3 libpcre3-dev libpcre++-dev zlib1g-dev libbz2-dev libxslt1-dev libxml2-dev libgd2-xpm-dev libgeoip-dev libgoogle-perftools-dev libperl-dev libssl-dev libcurl4-openssl-dev wget unzip make libssl-dev libpcre3-dev gcc make zlib1g-dev
+
 wget http://nginx.org/download/nginx-1.9.12.tar.gz
 
 tar xvf nginx-1.9.12.tar.gz
 
 cd nginx-1.9.12/
-sudo apt-get install -y curl build-essential make gcc libpcre3 libpcre3-dev libpcre++-dev zlib1g-dev libbz2-dev libxslt1-dev libxml2-dev libgd2-xpm-dev libgeoip-dev libgoogle-perftools-dev libperl-dev libssl-dev libcurl4-openssl-dev wget unzip make libssl-dev libpcre3-dev gcc make zlib1g-dev
 
 ./configure \
 --prefix=/etc/nginx                                                \
